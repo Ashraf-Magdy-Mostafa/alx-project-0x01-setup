64e666aff535ff771b8c793d674261742142f7b4
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-16 03:09:38 +0300

    added Header.tsx - applied it to Index & posts / users pages

---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/layout/Header";
+import React from 'react'
+
+function Users() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Users
