c7d7547e3271b258d2ccf143137eb048c1e2286e
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-16 05:13:57 +0300

    added users index.tsx page

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,21 @@
+import { UserProps } from "@/interfaces";
+const UserCard: React.FC<{ user: UserProps }> = ({ user }) => {
+    return (
+        <div className="border rounded-xl p-6 shadow-md hover:shadow-lg transition-all bg-white max-w-md mx-auto mb-4">
+            <h2 className="text-xl font-bold text-blue-600">{user.name}</h2>
+            <p className="text-gray-600">@{user.username}</p>
+            <p className="mt-2 text-sm text-gray-700">
+                <strong>Email:</strong> {user.email}<br />
+                <strong>Phone:</strong> {user.phone}<br />
+                <strong>Website:</strong> <a href={`https://${user.website}`} target="_blank" rel="noreferrer" className="text-blue-500 underline">{user.website}</a>
+            </p>
+            <div className="mt-4 text-sm text-gray-800">
+                <p><strong>Address:</strong> {user.address.street}, {user.address.suite}, {user.address.city} ({user.address.zipcode})</p>
+                <p><strong>Company:</strong> {user.company.name}</p>
+                <p className="italic text-gray-500">"{user.company.catchPhrase}"</p>
+            </div>
+        </div>
+    );
+};
+
+export default UserCard;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,12 +1,37 @@
 import Header from "@/components/layout/Header";
 import React from 'react'
+import { UserProps } from "@/interfaces";
+import UserCard from "@/components/common/UserCard";
 
-function Users() {
+interface UserPropsList {
+    posts: UserProps[] // [{},{},{}]
+
+}
+
+function Users({ posts }: UserPropsList) {
     return (
-        <div>
-            <Header />
-        </div>
+        <>
+            <div>
+                <Header />
+
+            </div>
+            <div className="p-6 grid grid-cols-1 md:grid-cols-2 min-h-screen place-items-center gap-6">
+                {posts.map((user) => (
+                    <UserCard key={user.id} user={user} />
+                ))}
+            </div>
+        </>
     )
 }
 
+export async function getStaticProps() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const posts = await response.json();
+
+    return {
+        props: {
+            posts,
+        },
+    };
+}
 export default Users
